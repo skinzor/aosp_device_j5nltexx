0aa5a63b62ec2d97a3938597ebdc9c4b4d7097fc
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-04-28 17:58:42 +0530

    added support J500F single sim variant

--- init/init_j5nlte.c
+++ init/init_j5nlte.c
@@ -61,7 +61,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.model", "SM-J500FN");
         property_set("ro.product.device", "j5nlte");
         
-    }else if (strstr(bootloader, "J500FXX")) {
+    }else if (strstr(bootloader, "J500F")) {
         /* SM-J500F single sim variant */
         property_set("ro.build.fingerprint", "samsung/j5ltexx/j5lte:5.1.1/LRX22G/J500FXXU1APB4:user/release-keys");
         property_set("ro.build.description", "j5ltexx-user 5.1.1 LMY48B J500FXXU1APB4 release-keys");
