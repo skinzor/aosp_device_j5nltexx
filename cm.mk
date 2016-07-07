ca286c990f92f93c3fbbe6c07c412a0b1c90eabc
Nick Verse <nikoskalomiris@gmail.com>
2016-07-07 04:23:06 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,5 +1,6 @@
-# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/j5nltexx/full_j5nltexx.mk)
 
