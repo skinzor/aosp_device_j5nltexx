a7b58d7da7c152edb42ea74b588d3ad665b3ce3f
#Henkate @xda <robertpopescu95@yahoo.com>
2016-12-29 08:56:48 +0200

    cm -> aosp

--- aosp.mk
+++ aosp.mk
@@ -2,8 +2,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/j5nltexx/full_j5nltexx.mk)
+$(call inherit-product, device/samsung/j5nltexx/aosp_j5nltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=j5nltexx TARGET_DEVICE=j5nlte
 
-PRODUCT_NAME := cm_j5nltexx
+PRODUCT_NAME := aosp_j5nltexx
